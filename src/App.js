874bc434989c5927fc11a5d977238b0fa679379a
Angelo <angelo.anselmet83@gmail.com>
2026-03-11 14:11:00 +0100

    Add logo SVG, web vitals reporting, and setup tests
    - Added a new SVG logo file to the project.
    - Implemented web vitals reporting functionality in a new file.
    - Created a setupTests.js file to include custom jest matchers for DOM assertions.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          SITE EN CONSTRUCTION
+        </p>
+        <a href="mailto:contact@bde42marseille.fr" className="contact-button">
+          CONTACT
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
